142888b2aded0ecdfab5b3a520c3157175efcd80
jjunyuongv <jjunyuongv@gmail.com>
2025-11-26 10:47:56 +0900

    프론트 cors 추가

--- config/cors.py
+++ config/cors.py
@@ -1,5 +1,5 @@
 """CORS 설정"""
-CORS_ORIGINS = ["http://localhost:3000", "http://localhost:5173"]
+CORS_ORIGINS = ["http://localhost:3000", "http://localhost:5173", "https://marryday-front.vercel.app"]
 CORS_CREDENTIALS = True
 CORS_METHODS = ["*"]
 CORS_HEADERS = ["*"]
